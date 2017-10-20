6fd2304cedde6608d4fdfb5971ca09f8a1a47ea7
liuxinhongg <644391351@qq.com>
2017-10-20 20:12:09 +0800

    add stark project

---
+++ vnshop/client/src/components/NavBread.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+                <a href="/">Home</a>
+                <!-- <span>Goods</span> -->
+                <!-- <span>商品详情</span> -->
+                <slot></slot>
+            </nav>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+  }
+</script>
